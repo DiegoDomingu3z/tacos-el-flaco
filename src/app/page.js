db2bbebbde0586aeb4b6f85be9bcf34373d7719e
Diego <diegodomdev@gmail.com>
2025-03-15 03:13:24 -0600

    la provider hot-fix

--- src/app/page.js
+++ src/app/page.js
@@ -1,10 +1,13 @@
 
+import { LanguageProvider } from "@/context/LanguegeContext";
 import HomePage from "@/pages/homepage";
 export default function Home() {
 
   return (
     <div className=" min-h-screen gap-16 ">
-      <HomePage />
+      <LanguageProvider>
+        <HomePage />
+      </LanguageProvider>
     </div>
   );
 }
